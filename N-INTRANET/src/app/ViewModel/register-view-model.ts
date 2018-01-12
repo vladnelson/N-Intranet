616855b293a94ef4239beb4dc58d7268f14271c9
valdnelson <didimilca@hotmail.com>
2018-01-12 16:47:25 +0100

    Creation du service CreateUtilisateur

---
+++ N-INTRANET/src/app/ViewModel/register-view-model.ts
@@ -0,0 +1,10 @@
+export class RegisterViewModel {
+  Civilite: string;
+  Nom: string ;
+  Prenom: string;
+  Login: string;
+  Email: string;
+  Password: string;
+  ConfirmPassword: string;
+  TelephoneP: string;
+}
